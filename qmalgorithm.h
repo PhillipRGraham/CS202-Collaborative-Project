d6cefd95ae67f70c3663fbdcd66174d862d02b7c
Phillip Reed Graham <pgraham7@hydra15.eecs.utk.edu>
2022-10-29 23:03:22 -0400

    Completely redesigned the header file

--- qmalgorithm.h
+++ qmalgorithm.h
@@ -1,48 +1,72 @@
 //  October 26, 2022
-//	Phillip Graham, Matthew Bringle, Ethen Eisenhower
+//	Phillip Graham
 //	The purpos of this project is to take in a file containing boolean
-//	expressions and simplify them according to users sepcifications. The
-//	progrma may only take a expression of maximum 32 input variables, and
-//	may only have one out put variable 
+//	expressions and simplify them according to the Quine-McCluskey alorithm.
+//	The program may only take a expression of maximum 32 input variables
+//	and may only have one output variable 
 
 using namespace std;
-#include <tree>
+#include <string>
 
-class truthTable ()
+//	Represents either a boolean variable or operation on two variables.
+class treeNode
 {
 	public:
-		truthTable (string);
+		treeNode(char);
+		~treeNode();
+
+		treeNode * getLeftChild ();
+		treeNode * getRightChild ();
+		treeNode * getParent ();
+		char getNodeContents ();
 	private:
-		int numberInputVar;
-		string userExpression;
-			//	This contains the user readable boolean expression provided.
-		tree <char> convertedExpression;
-			//	Contains what is essential the order of operation, in sequance of execution.
-			//	Each node of the tree is either 1, 0, +, *, or '.
-			//	These represent true, false, OR, AND, as well as NOT. With these we have a boolean complete expression.
-		void expressionConvertor ();
-		void initializeArrays ();
-			//	Essentially 
-		int * inputArr;
-			//	This vector stores the input to our given boolean expression
-			//	as an integer. Essentailly a function of 8 inputs would be 
-			//	stored as a an input 8 bit integer.
-		bool * outputArr;
-			//	This array stores the outputs to our given expression as
-			//	either a true or false, the given index correlates to a
-			//	given input in the inputArr.
+
+		char nodeContents;
+		treeNode * parent;
+		treeNode * leftChild;
+		treeNode * rightChild;
 };
 
-class qmAlgorithm ()
+//	Represents a boolean expresion in the form of a binary tree
+class equationTree
 {
 	public:
-		
+		equationTree();
+		~equationTree();
+			
+		//	Travels down tree and executes boolean expression
+		bool calculate (int, char *);
 	private:
-		truthTable * truthTableArr;
-			//	This array just contains all of our described truth tables.
-			//	Using these inputs and outputs, we will be able to determine a minimum sup of products
-			//	using the Quine McClusky algorithm
-		string * msopArr;
-		void addMsop (string);
-		void printMsopArr();
+		treeNode * rootNode;
 };
+
+//	Contains both user readable and function readbale boolean expression 
+class booleanExpression
+{
+	public:
+		booleanExpression (string);
+		~booleanExpression ();
+
+		int getNumberPossibleInputs();
+		bool getTableOutput (int);
+	private:
+		string userReadableExpression;
+		char * inputVariableArr;
+		equationTree treeRepresentation;
+
+		void generateTable ();
+		bool * tableOutputArr;
+};
+
+class qmAlgorithm
+{
+	public:
+		qmAlgorithm ();
+		~qmAlgorithm ();
+	private:
+		booleanExpression * expressionArr;
+		string generateMSOP(booleanExpression &);
+		void addExpression ();
+		int numExpression;
+};
+
